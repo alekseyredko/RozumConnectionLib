bf306b78ef390446aa5122f19814e34399a2c117
Aleksey Redko <alekseyredko1@yandex.ru>
2019-04-07 09:30:39 +0300

    update gripper class

--- Gripper.cs
+++ Gripper.cs
@@ -6,9 +6,11 @@ namespace RozumConnectionLib
     public class Gripper
     {
         [JsonProperty("name")]
-        public string Name { get; set; }
-        [JsonProperty("tcp")]
-        public Position Tcp { get; set; }
+        public string Name { get; set; }        
+        [JsonProperty("point")]
+        public Point Point { get; set; }
+        [JsonProperty("rotation")]
+        public Rotation Rotation { get; set; }
         [JsonProperty("shape")]
         public List<CapsuleObstacle> Shape { get; set; }
 
